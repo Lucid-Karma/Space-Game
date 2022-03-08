eaf354c5826b69235d821769405c1ec4fd9f7371
Beyzanur <beyzaalways@gmail.com>
2022-03-08 23:42:00 +0300

    More Trouble

--- Destroy Planets/Assets/Scripts/Player.cs
+++ Destroy Planets/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{/*
+{
     private Rigidbody rigidb;
 	public Rigidbody Rigidb 
 	
@@ -21,13 +21,17 @@ public class Player : MonoBehaviour
 	
     public static float forwardForce = 2000f;
 	public float MoveSpeed = 500;
+    public float RotSpeed = 100;
 
 	//public static int point = 0;
 
 
     private void FixedUpdate()
     {
-       Rigidb.AddForce(0, 0, forwardForce * Time.fixedDeltaTime);
+       //Rigidb.AddForce(0, 0, forwardForce * Time.fixedDeltaTime);
+       float VI = Input.GetAxis("Vertical") * RotSpeed;
+       float HI = Input.GetAxis("Horizontal") * RotSpeed;
+       transform.Rotate(VI, HI, 0 * RotSpeed * Time.fixedDeltaTime);
 
        if(Input.GetKey(KeyCode.X))
        {
@@ -39,13 +43,17 @@ public class Player : MonoBehaviour
            forwardForce = forwardForce - 800f;
        } 
 
-	   Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+	   Vector3 Posinput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+       //Vector3 Rotinput = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
 	   
-	   Rigidb.velocity = input * MoveSpeed* Time.fixedDeltaTime; // velocity can cause problems in some case.		
-    }*/
+	   Rigidb.velocity = Posinput * MoveSpeed * Time.fixedDeltaTime; // velocity can cause problems in some case.	
+       //Rigidb.AddForce(VI, HI, HI * forwardForce * Time.fixedDeltaTime);
+       //transform.Translate(Posinput * MoveSpeed * Time.fixedDeltaTime);
+       //transform.Rotate(VI, HI, 0 * RotSpeed * Time.fixedDeltaTime);	
+    }
 
 
-
+/*
     public float speed;
     public float maxspeed;
     public float minspeed;
@@ -54,9 +62,9 @@ public class Player : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position += transform.forward * Time.deltaTime * speed;
+        transform.position += transform.forward * Time.fixedDeltaTime * speed;
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -68,39 +76,41 @@ public class Player : MonoBehaviour
             if (speed > minspeed) speed--;
         }
 
-        speed -= transform.forward.y * Time.deltaTime * 50;
+        speed -= transform.forward.y * Time.fixedDeltaTime * 10;
         if (speed < minspeed / 2) speed = minspeed;
         if (speed > maxspeed * 2) speed = maxspeed;
 
         
         if (Input.GetKey(KeyCode.LeftArrow)) {
-            transform.Rotate(Vector3.forward*Time.deltaTime*rotspeed1);
+            transform.Rotate(-Vector3.up * Time.fixedDeltaTime * rotspeed2);
+            transform.Translate(Vector3.left*Time.fixedDeltaTime*rotspeed1);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(Vector3.back * Time.deltaTime * rotspeed1);
+            transform.Rotate(-Vector3.down * Time.fixedDeltaTime * rotspeed2);
+            transform.Translate(Vector3.right * Time.fixedDeltaTime * rotspeed1);
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Rotate(Vector3.right * Time.deltaTime * rotspeed1);
+            transform.Rotate(Vector3.left * Time.fixedDeltaTime * rotspeed1);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Rotate(Vector3.left * Time.deltaTime * rotspeed1);
-        }
+            transform.Rotate(Vector3.right * Time.fixedDeltaTime * rotspeed1);
+        }*/
 
-        if (Input.GetKey(KeyCode.A))
+        /*if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(-Vector3.up * Time.deltaTime * rotspeed2);
+            transform.Rotate(-Vector3.up * Time.fixedDeltaTime * rotspeed2);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.down * Time.deltaTime * rotspeed2);
+            transform.Rotate(-Vector3.down * Time.fixedDeltaTime * rotspeed2);
         }
 
-    }
+    }*/
 }
